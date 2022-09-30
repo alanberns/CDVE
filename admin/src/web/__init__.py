4f47743d87ed59f7d9a4691536eba9d8e5cac26a
Alan Berns <alan_berns@yahoo.com.ar>
2022-09-29 21:33:59 -0300

    feat: Configuración acceso a BBDD

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import render_template
 
+from src.core import database
 from src.web.config import config
 from src.web.helpers import handlers
+from src.web.controllers.usuarios import usuario_blueprint
 
 
 def create_app(env="development", static_folder="static"):
@@ -10,9 +12,14 @@ def create_app(env="development", static_folder="static"):
 
     app.config.from_object(config[env])
 
+    database.init_app(app)
+    
     @app.get("/")
     def home():
         return render_template("home.html")
+
+
+    app.register_blueprint(usuario_blueprint)
 
     app.register_error_handler(404, handlers.not_found_error)
     app.register_error_handler(500, handlers.internal_server_error)
