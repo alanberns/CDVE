4bfa8487569553678b565273865d0d7219a9dc2e
Mateo Elizondo <mateo.elizondo@hotmail.com>
2022-10-08 23:24:28 -0300

    feat: clase socio

---
+++ admin/src/core/board/socio.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from src.core.database import db
+
+
+class Socio(db.Model):
+
+    __tablename__ = "socios"
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    id_usuario = db.Column(db.Integer,db.ForeignKey("usuarios.id"))
+
+    tipo_documento = db.Column(db.String(255))
+    numero_documento = db.Column(db.Integer())
+    genero = db.Column(db.String(255))
+    numero_socio = db.Column(db.String(255))
+    direccion = db.Column(db.String(255))        
+    telefono = db.Column(db.Integer())
+    email = db.Column(db.String(255))
+
+    #falta relacion con cuota y disciplina
