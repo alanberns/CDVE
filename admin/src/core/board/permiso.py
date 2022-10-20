0a7379d1094fbfa948b02463bcff2a46e121c2d9
Pablo <pablo.d.ormeno@gmail.com>
2022-10-20 14:38:19 -0500

    Actualizo archivos de modulo disciplina

--- admin/src/core/board/permiso.py
+++
@@ -1,7 +0,0 @@
-from src.core.database import db
-
-class Permiso(db.Model):
-
-    __tablename__ = "permisos"
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    nombre = db.Column(db.String(255))
