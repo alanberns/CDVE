b7bf8815d574a279325d5c30498f589a5ec37993
diegoag30 <diego.pablo.aguilar@gmail.com>
2022-10-21 00:37:56 -0300

    layout: boton usuario en nav ahora exige permiso

--- admin/src/core/board/__init__.py
+++ admin/src/core/board/__init__.py
@@ -334,9 +334,9 @@ def pagar_cuota_by_id(id_cuota):
     return cuota
 
 
-# def user_get_permisos(user_id):
-#     roles = Rol.query.join(
-#         Usuario.roles).filter_by(id=user_id).all()
-#     permisos = list(
-#         {permiso.nombre for rol in roles for permiso in rol.permisos})
-#     return permisos
+def user_get_permisos(usuario_id):
+    roles = Rol.query.join(Usuario_tiene_rol).filter_by(
+        usuario_id=usuario_id).all()
+    permisos = list(
+        {permiso.nombre for rol in roles for permiso in rol.permisos})
+    return permisos
