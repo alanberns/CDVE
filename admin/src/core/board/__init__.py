315681a8f033cfab1a9f8399c380c0b1a91f42de
Alan Berns <alan_berns@yahoo.com.ar>
2022-11-08 12:55:05 -0300

    fix: busqueda por email usando like

--- admin/src/core/board/__init__.py
+++ admin/src/core/board/__init__.py
@@ -105,10 +105,10 @@ def filter_usuarios(email, activo, page=1, per_page=10):
                 Usuario.id.asc()).paginate(page=page, per_page=per_page, error_out=False)
     else:
         if (activo != ""):
-            usuarios = Usuario.query.filter_by(activo=activo, email=email).order_by(
+            usuarios = Usuario.query.filter(Usuario.activo == activo, Usuario.email.like(email +"%")).order_by(
                 Usuario.id.asc()).paginate(page=page, per_page=per_page, error_out=False)
         else:
-            usuarios = Usuario.query.filter_by(email=email).order_by(
+            usuarios = Usuario.query.filter(Usuario.email.like(email +"%")).order_by(
                 Usuario.id.asc()).paginate(page=page, per_page=per_page, error_out=False)
     return usuarios
 
