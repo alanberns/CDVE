b2573585b40c5b81791d592715d0902899e062c9
diegoag30 <diego.pablo.aguilar@gmail.com>
2022-11-12 21:58:35 -0300

    feat: filtro busqueda y busqueda nro_socio string
    - Si se busca por string con el fitlro activo de nro_socio, entonces aparece un mensaje de error.
    - Se mantiene el filtro luego de buscar, para facilitar busquedas repetitivas

--- admin/src/web/controllers/pagos.py
+++ admin/src/web/controllers/pagos.py
@@ -24,8 +24,11 @@ pago_blueprint = Blueprint(
 def pagos_index():
     form = PagoSearchForm()
     page = request.args.get('page', default=1, type=int)
+    filter = request.args.get('filter', default=0, type=int)
+    error = request.args.get('error', type=int)
+    form.select_busqueda.data = filter
     pagos = board.list_pagos(page)
-    return render_template("pagos/pagos.html", pagos=pagos, form=form)
+    return render_template("pagos/pagos.html", pagos=pagos, form=form, error=error)
 
 
 @pago_blueprint.post("/searching")
@@ -47,7 +50,7 @@ def pagos_searching():
         try:
             int(busqueda)
         except ValueError:
-            abort(400)
+            return redirect(url_for("pagos.pagos_index", filtro=filtro, error=1))
     return redirect(url_for("pagos.pagos_search", page=page, filtro=filtro, busqueda=busqueda))
 
 
