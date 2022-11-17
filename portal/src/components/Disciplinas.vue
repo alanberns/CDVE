134ffdefa214618f754df90f4bc885c39f0cf27d
Alan Berns <alan_berns@yahoo.com.ar>
2022-11-17 18:39:32 -0300

    feat: primer prueba con vue parte portal

---
+++ portal/src/components/Disciplinas.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>NOMBRE</th>
+            <th>CATEGORIA</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="disciplina in disciplinas" :key="disciplina.id">
+            <td>{{ disciplina.name }}</td>
+            <td>{{ disciplina.categoria }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
+  <script>
+  import axios from "axios";
+  export default {
+    name: "Disciplinas",
+    data() {
+      return {
+        disciplinas: [],
+      };
+    },
+    created() {
+      let url = "http://127.0.0.1:5000/api/club/disciplines";
+  
+      axios
+        .get(url)
+        .then((response => (this.disciplinas = response.data)))
+        .catch((error) => console.log(error));
+    },
+  };
+  </script>
+  
+  <style></style>
---
+++ portal/src/components/Usuario.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>NOMBRE</th>
+            <th>EMAIL</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{{ user_info.user }}</td>
+            <td>{{ user_info.email }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
+  <script>
+  import axios from "axios";
+  export default {
+    name: "Usuario",
+    data() {
+      return {
+        user_info: [],
+      };
+    },
+    created() {
+      let url = "http://127.0.0.1:5000/api/me/profile";
+  
+      axios
+        .get(url)
+        .then((response => (this.user_info = response.data)))
+        .catch((error) => console.log(error));
+    },
+  };
+  </script>
+  
+  <style></style>
