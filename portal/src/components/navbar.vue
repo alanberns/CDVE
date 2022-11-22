e1e1d11305604c7504ef9b0b11457646d4e7029d
Alan Berns <alan_berns@yahoo.com.ar>
2022-11-22 07:48:09 -0300

    merge development

--- portal/src/components/navbar.vue
+++ portal/src/components/navbar.vue
@@ -1,35 +1,36 @@
 <template>
-  <div class="navbar-fixed">
-    <nav>
-      <div class="nav-wrapper blue-grey darken-4">
-        <a href="#" class="brand-logo right"
-          ><i class="material-icons right"
-            ><i class="material-icons right">account_circle</i></i
-          ></a
-        >
-        <ul id="nav-mobile" class="left hide-on-med-and-down">
-          <li><router-link to="/disciplinas">Disciplinas</router-link></li>
-          <li><router-link to="/user">User</router-link></li>
-          <li><router-link to="/Login">Login</router-link></li>
-          <li><router-link to="/pagos">Pagos</router-link></li>
-        </ul>
-      </div>
-    </nav>
-  </div>
-</template>
-
-<script>
-export default {
-  name: "NavbarComponent",
-};
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.navbar-fixed {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-}
-</style>
+    <div class="navbar-fixed">
+      <nav>
+        <div class="nav-wrapper blue-grey darken-4">
+          <a href="#" class="brand-logo right"
+            ><i class="material-icons right"
+              ><i class="material-icons right">account_circle</i></i
+            ></a
+          >
+          <ul id="nav-mobile" class="left hide-on-med-and-down">
+            <li><router-link to="/disciplinas">Disciplinas</router-link></li>
+            <li><router-link to="/user">User</router-link></li>
+            <li><router-link to="/Login">Login</router-link></li>
+            <li><router-link to="/pagos">Pagos</router-link></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "NavbarComponent",
+  };
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  .navbar-fixed {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+  }
+  </style>
+  
