99cafcc73c9ea0b3c9ddc8df41e5e0d0a05d8b92
Alan Berns <alan_berns@yahoo.com.ar>
2022-11-23 14:14:17 -0300

    style: completado vista usuarios

--- portal/src/components/Usuario.vue
+++ portal/src/components/Usuario.vue
@@ -1,16 +1,29 @@
 <template>
     <div>
-        <table>
+        <br><br>
+        <table class="responsive-table centered highlight ">
             <thead>
                 <tr>
+                    <th>N° SOCIO</th>
                     <th>NOMBRE</th>
                     <th>EMAIL</th>
+                    <th>TIPO DOCUMENTO</th>
+                    <th>N° DOCUMENTO</th>
+                    <th>GENERO</th>
+                    <th>TELEFONO</th>
+                    <th>DIRECCIÓN</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
+                    <td>{{user_info.number}}</td>
                     <td>{{ user_info.user }}</td>
                     <td>{{ user_info.email }}</td>
+                    <td>{{user_info.document_type}}</td>
+                    <td>{{user_info.document_number}}</td>
+                    <td>{{user_info.gender}}</td>
+                    <td>{{user_info.phone}}</td>
+                    <td>{{user_info.address}}</td>
                 </tr>
             </tbody>
         </table>
