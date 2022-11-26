f8ced05394a3f1bd480c77d4f84b74920c12cb69
diegoag30 <diego.pablo.aguilar@gmail.com>
2022-11-25 23:06:50 -0300

    feat: portal obtenidas disciplinas de usuario

--- portal/src/components/GenerarPago.vue
+++ portal/src/components/GenerarPago.vue
@@ -1,19 +1,60 @@
 <template>
   <br />
   <div class="container">
-    <SubirArchivo></SubirArchivo>
+    <div class="row valign-wrapper">
+      <div class="col l6">
+        <p class="flow-text">Seleccione la disciplina que desea pagar</p>
+      </div>
+      <div class="col l6">
+        <Select2
+          v-model="selected"
+          :options="myOptions"
+          :settings="{ width: '50%' }"
+          @select="mySelectEvent($event)"
+        />
+      </div>
+    </div>
+
+    <SubirArchivo v-if="selected"></SubirArchivo>
   </div>
 </template>
 
 <script>
 import SubirArchivo from "./SubirArchivo.vue";
+import { apiService } from "../apiService";
 
 export default {
   name: "ImportarPago",
+  data() {
+    return {
+      myOptions: [],
+      selected: null,
+    };
+  },
+  mounted() {
+    this.getDisciplinas();
+  },
   components: {
     SubirArchivo,
   },
-  methods: {},
+  methods: {
+    mySelectEvent(e) {
+      this.selected = e.text;
+    },
+    async getDisciplinas() {
+      await apiService
+        .get("/me/disciplines", {
+          headers: {
+            Authorization: `${localStorage.getItem("token")}`,
+          },
+        })
+        .then((response) => {
+          const data = response.data.data;
+          Object.values(data).forEach((item) => this.myOptions.push(item.name));
+        })
+        .catch((error) => console.log(error));
+    },
+  },
 };
 </script>
 
--- portal/src/main.js
+++ portal/src/main.js
@@ -4,5 +4,10 @@ import router from "./router";
 import "materialize-css/dist/css/materialize.min.css";
 import "material-design-icons-iconfont";
 import { pinia } from "./persistStore";
+import Select2 from "vue3-select2-component";
 
-createApp(App).use(router).use(pinia).mount("#app");
+createApp(App)
+  .component("Select2", Select2)
+  .use(router)
+  .use(pinia)
+  .mount("#app");
